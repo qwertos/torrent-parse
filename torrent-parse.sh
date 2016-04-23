07d20a73397c314ea85f5e0a247e8a413ac1012a
Jen Herting <jen@herting.cc>
2016-04-23 13:20:36 -0400

    asdf

--- torrent-parse.sh
+++ torrent-parse.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-TORRENT_PATH=$1
+TORRENT_PATH="$1"
 
-full_length=$(wc -c $TORRENT_PATH | cut -d' ' -f1)
+full_length=$(wc -c "$TORRENT_PATH" | cut -d' ' -f1)
 position=0
 
 depth=0
 echo "{"
 
-read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 (( position += 1 ))
 if [[ $read_char == 'd' ]] ; then
 	(( depth ++ ))
@@ -18,21 +18,21 @@ while [[ $position -lt $full_length ]] ; do
 		
 	# GET KEY LENGTH
 	key_len=0
-	read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+	read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 	(( position += 1 ))
 	while [[ $read_char != ':' ]] ; do 
 		(( key_len *= 10 ))
 		(( key_len += read_char ))
-		read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+		read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 		(( position += 1 ))
 	done
 
 	# GET KEY
-	key=$(dd if=${TORRENT_PATH} bs=1 count=${key_len} skip=$position 2> /dev/null)
+	key=$(dd "if=${TORRENT_PATH}" bs=1 count=${key_len} skip=$position 2> /dev/null)
 	(( position += key_len ))
 
 	# CHECK DEPTH
-	read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+	read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 	if [[ $read_char == 'd' ]] ; then 
 		# OUTPUT DATA
 		i=$depth
@@ -51,33 +51,33 @@ while [[ $position -lt $full_length ]] ; do
 	fi
 
 
-	#read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+	#read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 	if [[ $read_char =~ [0-9] ]] ; then
 		# GET VALUE LENGTH
 		value_len=0
-		read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+		read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 		(( position += 1 ))
 		while [[ $read_char != ':' ]] ; do 
 			(( value_len *= 10 ))
 			(( value_len += read_char ))
-			read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+			read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 			(( position += 1 ))
 		done
 
 		# GET VALUE
-		value=$(dd if=${TORRENT_PATH} bs=1 count=${value_len} skip=$position 2> /dev/null)
+		value=$(dd "if=${TORRENT_PATH}" bs=1 count=${value_len} skip=$position 2> /dev/null)
 		(( position += value_len ))
 	
 	elif [[ $read_char =~ 'i' ]] ; then
 		value_len=0
 		value=0
-		read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+		read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 		(( position += 1 ))
 		while [[ $read_char != 'e' ]] ; do
 			(( value_len ++ ))
 			(( value *= 10 ))
 			(( value += read_char ))
-			read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+			read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 			(( position += 1 ))
 		done
 	fi
@@ -95,7 +95,7 @@ while [[ $position -lt $full_length ]] ; do
 	printf '"%s": "%s"' "$key" "$value"
 	echo
 
-	read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+	read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 	while [[ $read_char == 'e' ]] ; do
 		# OUTPUT DATA
 		(( depth -- ))
@@ -108,7 +108,7 @@ while [[ $position -lt $full_length ]] ; do
 		echo "}"
 
 		(( position ++ ))
-		read_char=$(dd if=${TORRENT_PATH} bs=1 count=1 skip=$position 2> /dev/null)
+		read_char=$(dd "if=${TORRENT_PATH}" bs=1 count=1 skip=$position 2> /dev/null)
 	done
 done
 
